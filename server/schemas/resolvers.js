5411e412d9c16dc976257c19229ee82113c393fb
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-04-07 12:24:05 -0500

    ran npm update on all main, client and server directories

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,22 +4,46 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    // get a single user by either their id or their username
-        async getSingleUser({ user = null, params }, res) {
-        const foundUser = await User.findOne({
-          $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
-        });
-
-        if (!foundUser) {
-          return res.status(400).json({ message: 'Cannot find a user with this id!' });
-        }
-
-        res.json(foundUser);
+    me: async (parent, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('thoughts');
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
+
   },
 
   Mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+
+
     
+    saveBook: async (parent, { authors, description, title, bookId, image, link }) => {
+      const book = await Thought.create({ thoughtText, thoughtAuthor });
+
+      await User.findOneAndUpdate(
+        { username: thoughtAuthor },
+        { $addToSet: { thoughts: book._id } }
+      );
+
+      return book;
+    },
   },
 };
 
